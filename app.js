9ed18224dd7a5438c946d7451ee795ebe22ecc32
c3h3 <c3h3.tw@gmail.com>
2017-01-22 01:29:10 +0800

    hi bot!

---
+++ app.js
@@ -0,0 +1,50 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', function (session) {
+    session.send("%s, I heard: %s", session.userData.name, session.message.text);
+    session.send("Say something else...");
+});
+
+// Install First Run middleware and dialog
+bot.use(builder.Middleware.firstRun({ version: 1.0, dialogId: '*:/firstRun' }));
+bot.dialog('/firstRun', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        // We'll save the users name and send them an initial greeting. All 
+        // future messages from the user will be routed to the root dialog.
+        session.userData.name = results.response;
+        session.endDialog("Hi %s, say something to me and I'll echo it back.", session.userData.name); 
+    }
+]);
+
+
+  
+
+// //=========================================================
+// // Bots Dialogs
+// //=========================================================
+
+// bot.dialog('/', function (session) {
+//     session.send("Hello World");
+// });
